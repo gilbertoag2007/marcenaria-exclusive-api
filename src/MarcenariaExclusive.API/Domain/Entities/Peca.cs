d3ef9bebfccd476d3b2c1b1c3c5d1a62bcb3f56b
Gilberto Cunha <gilbertoag2007@gmail.com>
2025-02-15 10:57:58 -0300

    Atualizar o Peca.cs
    Inclusão do construtor com argumentos.

--- src/MarcenariaExclusive.API/Domain/Entities/Peca.cs
+++ src/MarcenariaExclusive.API/Domain/Entities/Peca.cs
@@ -1,34 +1,42 @@
-﻿using MarcenariaExclusive.API.Domain.Enums;
+using MarcenariaExclusive.API.Domain.Enums;
 
 /// <summary>
-/// Representa os atributos de uma peça de mdf calculada de acordo com as especificações do armário.
+/// Representa os atributos de uma peça de MDF calculada de acordo com as especificações do armário.
 /// </summary>
 public class Peca
 {
     /// <summary>
     /// Largura da peça em centímetros.
     /// </summary>
-    public int largura { get; set; }
+    public int Largura { get; set; }
 
     /// <summary>
     /// Altura da peça em centímetros.
     /// </summary>
-    public int altura { get; set; }
+    public int Altura { get; set; }
 
     /// <summary>
     /// Espessura da peça.
     /// </summary>
-    public Espessura espessura { get; set; }
+    public Espessura Espessura { get; set; }
 
     /// <summary>
     /// Finalidade da peça.
     /// </summary>
-    public FinalidadePeca tipoPeca { get; set; }
+    public FinalidadePeca TipoPeca { get; set; }
 
     /// <summary>
     /// Inicializa uma nova instância da classe <see cref="Peca"/>.
     /// </summary>
-    public Peca()
+    /// <param name="largura">Largura da peça em centímetros.</param>
+    /// <param name="altura">Altura da peça em centímetros.</param>
+    /// <param name="espessura">Espessura da peça.</param>
+    /// <param name="tipoPeca">Finalidade da peça.</param>
+    public Peca(int largura, int altura, Espessura espessura, FinalidadePeca tipoPeca)
     {
+        Largura = largura;
+        Altura = altura;
+        Espessura = espessura;
+        TipoPeca = tipoPeca;
     }
 }
