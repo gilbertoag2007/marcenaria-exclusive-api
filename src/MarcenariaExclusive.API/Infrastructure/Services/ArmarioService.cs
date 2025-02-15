f4018d7eb346c3f6854a351e456dab8b82d082a1
Gilberto Cunha <gilbertoag2007@gmail.com>
2025-02-15 12:41:23 -0300

    Atualizar o ArmarioService.cs
    Definição das peças: laterais, base, topo, acabamentos e fundo.

--- src/MarcenariaExclusive.API/Infrastructure/Services/ArmarioService.cs
+++ src/MarcenariaExclusive.API/Infrastructure/Services/ArmarioService.cs
@@ -12,7 +12,24 @@ namespace MarcenariaExclusiveAPI.Infrastructure.Services
         {
 
             Console.WriteLine("CHEGOU NO SERVICE.");
-            throw new DimensoesException(" AS ESPECIFICAÇÕES ESTÃO FORA DE UMA PROPORÇÃO RAZOAVEL PARA O PROJETO");
+          //  throw new DimensoesException(" AS ESPECIFICAÇÕES ESTÃO FORA DE UMA PROPORÇÃO RAZOAVEL PARA O PROJETO");
+
+PlanoCorte planoCorte = new PlanoCorte();
+planoCorte.Armario = armário;
+
+Peca laterais = new Peca(armario.Largura, armário.Altura, Espessura.Milimetros15,2 FinalidadePeca.Lateral);
+
+Peca base = new Peca(armario.Largura -3, armário.Altura, Espessura.Milimetros15,1 FinalidadePeca.Base);// redução de 3cm por conta da espessura das duas peças laterais
+
+Peca topo = new Peca(armario.Largura -3, armário.Altura, Espessura.Milimetros15,1 FinalidadePeca.Topo);// redução de 3cm por conta da espessura das duas peças laterais
+
+Peca acabamentoInferior = new Peca(armario.Largura, 7, Espessura.Milimetros15,1 FinalidadePeca.AcabamentoInferior);
+
+Peca acabamentoSuperior = new Peca(armario.Largura, 7, Espessura.Milimetros15,1 FinalidadePeca.AcabamentoSuperior);
+
+Peca fundo = new Peca(armario.Largura, armário.Altura, Espessura.Milimetros6,1 FinalidadePeca.Fundo);
+
+
 
 
 
